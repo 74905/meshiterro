4dc254614631531f79c2a00470303870f8383129
74905 <hurandall@yahoo.co.jp>
2020-11-14 11:45:16 +0000

    [finish]4-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!,except: [:top]
   before_action :conhigure_permitted_parameters, if: :devise_controller?
   
   def afetr_sign_in_path_for(resource)
