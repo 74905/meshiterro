9dc0bfc6556b3e678baf700c3295aac8e2fcd5c5
74905 <hurandall@yahoo.co.jp>
2020-11-14 09:43:26 +0000

    [start]4-4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :conhigure_permitted_parameters, if: :devise_controller?
   
+  def afetr_sign_in_path_for(resource)
+    post_images_path
+  end
   protected
   
   def conhigure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
   
-  has_many :post_images, dependent: :destroy
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9a593dc88a53378ac61aa095343d10131a280d1e7795993d784d83ea493e325e8739bd8df17fb2119e844d039db30f652a9d25e7ff0aa77cbfdc5791313ed5fc'
